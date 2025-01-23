ed6f07530092487b3bd0d422eb080d71d7ce79c2
DigCore <digcore@163.com>
2025-01-23 21:26:22 +0800

    add LoginView & MainView

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import LoginView from '../views/login/LoginView.vue'
+import MainView from '../views/main/MainView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      component: MainView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: MainView
+    },
+    // {
+    //   path: '/main',
+    //   name: 'main',
+    //   component: () => import('../views/main/MainView.vue')
+    // },
+  ]
+})
+
+export default router
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { fileURLToPath, URL } from 'node:url'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/vite-vue3/',
@@ -8,5 +10,10 @@ export default defineConfig({
   // build输出的目录设置为gh-pages
   build: {
     outDir: 'gh-pages'
-  }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 })
