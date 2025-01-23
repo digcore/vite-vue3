7449db9e52fdb5fca90dc4595ebdcc5dee3ea1f4
DigCore <digcore@163.com>
2025-01-23 21:42:13 +0800

    路由改用hash模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 import LoginView from '../views/login/LoginView.vue'
 import MainView from '../views/main/MainView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
