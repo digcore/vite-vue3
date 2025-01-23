a0004ba7db661e571f6aabf43b1e9accef23a3ca
DigCore <digcore@163.com>
2025-01-23 18:36:38 +0800

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/vite-vue3/',
   plugins: [vue()],
+  // build输出的目录设置为gh-pages
+  build: {
+    outDir: 'gh-pages'
+  }
 })
